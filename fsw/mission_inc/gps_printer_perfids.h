a6ec71111057cb6ec60c0c3df38408f638f1e0d3
Jacob Killelea <jkillelea@protonmail.ch>
2019-06-26 09:58:20 -0700

    add functions for printing out messages

---
+++ fsw/mission_inc/gps_printer_perfids.h
@@ -0,0 +1,6 @@
+#ifndef _GPS_PRINTER_PERFIDS_H_
+#define _GPS_PRINTER_PERFIDS_H_
+
+#define GPS_PRINTER_PERFID (93)
+
+#endif // _GPS_PRINTER_PERFIDS_H_
---
+++ fsw/src/gps_printer.h
@@ -0,0 +1,7 @@
+#ifndef _GPS_PRINTER_H_
+#define _GPS_PRINTER_H_
+
+void GPS_PRINTER_Main(void);
+void GPS_PRINTER_Init(void);
+
+#endif // _GPS_PRINTER_H_
