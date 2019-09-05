a400fb07ed0d34f049aa3545a02e3025c2c55916
Jacob Killelea <Jkillelea@protonmail.ch>
2019-09-04 17:12:52 -0700

    remove extra printfs.
    Wrapping case statements in blocks somehow needed for compilation?

--- fsw/src/gps_printer.c
+++ fsw/src/gps_printer.c
@@ -21,41 +21,39 @@ void GPS_PRINTER_Main(void) {
         CFE_ES_PerfLogEntry(GPS_PRINTER_PERFID);
 
         if (status == CFE_SUCCESS) {
-            OS_printf("GPS_PRINTER: got a gps message\n");
-            
             CFE_SB_MsgId_t msgId = CFE_SB_GetMsgId(gpsMessage);
-            
+
             switch (msgId) {
-            case GPS_READER_GPS_INFO_MSG:
-                OS_printf("GPS_PRINTER: GPS_READER_GPS_INFO_MSG Message (0x%x)\n", msgId);
+            case GPS_READER_GPS_INFO_MSG: {
                 GpsInfoMsg_t *gpsInfo = (GpsInfoMsg_t *) gpsMessage;
                 print_info(&gpsInfo->gpsInfo);
                 break;
-            case GPS_READER_GPS_GPGGA_MSG:
-                OS_printf("GPS_PRINTER: GPS_READER_GPS_GPGGA_MSG Message (0x%x)\n", msgId);
+            }
+            case GPS_READER_GPS_GPGGA_MSG: {
                 GpsGpggaMsg_t *gpsGpgga = (GpsGpggaMsg_t *) gpsMessage;
                 print_gpgga(&gpsGpgga->gpsGpgga);
                 break;
-            case GPS_READER_GPS_GPGSA_MSG:
-                OS_printf("GPS_PRINTER: GPS_READER_GPS_GPGSA_MSG Message (0x%x)\n", msgId);
+            }
+            case GPS_READER_GPS_GPGSA_MSG: {
                 GpsGpgsaMsg_t *gpsGpgsa = (GpsGpgsaMsg_t *) gpsMessage;
                 print_gpgsa(&gpsGpgsa->gpsGpgsa);
                 break;
-            case GPS_READER_GPS_GPGSV_MSG:
-                OS_printf("GPS_PRINTER: GPS_READER_GPS_GPGSV_MSG Message (0x%x)\n", msgId);
+            }
+            case GPS_READER_GPS_GPGSV_MSG: {
                 GpsGpgsvMsg_t *gpsGpgsv = (GpsGpgsvMsg_t *) gpsMessage;
                 print_gpgsv(&gpsGpgsv->gpsGpgsv);
                 break;
-            case GPS_READER_GPS_GPRMC_MSG:
-                OS_printf("GPS_PRINTER: GPS_READER_GPS_GPRMC_MSG Message (0x%x)\n", msgId);
+            }
+            case GPS_READER_GPS_GPRMC_MSG: {
                 GpsGprmcMsg_t *gpsGprmc = (GpsGprmcMsg_t *) gpsMessage;
                 print_gprmc(&gpsGprmc->gpsGprmc);
                 break;
-            case GPS_READER_GPS_GPVTG_MSG:
-                OS_printf("GPS_PRINTER: GPS_READER_GPS_GPVTG_MSG Message (0x%x)\n", msgId);
+            }
+            case GPS_READER_GPS_GPVTG_MSG: {
                 GpsGpvtgMsg_t *gpsGpvtg = (GpsGpvtgMsg_t *) gpsMessage;
                 print_gpvtg(&gpsGpvtg->gpsGpvtg);
                 break;
+            }
             default:
                 OS_printf("GPS_PRINTER: Unkown message ID 0x%x\n", msgId);
                 break;
@@ -67,7 +65,7 @@ void GPS_PRINTER_Main(void) {
 void GPS_PRINTER_Init(void) {
     CFE_ES_RegisterApp();
     CFE_EVS_Register(NULL, 0, CFE_EVS_BINARY_FILTER);
-    
+
     OS_printf("GPS_PRINTER: Startup.");
 
     CFE_SB_CreatePipe(&gpsPipe, 10, "GPS_PRINTER_PIPE");
